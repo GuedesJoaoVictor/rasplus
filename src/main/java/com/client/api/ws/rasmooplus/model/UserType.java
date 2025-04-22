eb23b1b03fd6a657d5a3bb2ab1429ca972d3ddeb
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-22 14:40:10 -0300

    feat: Criando entidade UserType
    Criando usertype para mapeameno no banco

---
+++ src/main/java/com/client/api/ws/rasmooplus/model/UserType.java
@@ -0,0 +1,44 @@
+package com.client.api.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UserType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_type")
+    private Long id;
+    private String name;
+    private String description;
+
+    public UserType() {}
+
+    public UserType(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
