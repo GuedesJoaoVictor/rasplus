60984c4a1a4da5cdedec578b2e791317b37ef51d
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-22 16:38:12 -0300

    feat: Adicionando classe UserPaymentInfo
    Criando classe UserPaymentInfo e adicionando seus relacionamentos.

---
+++ src/main/java/com/client/api/ws/rasmooplus/model/UserPaymentInfo.java
@@ -0,0 +1,105 @@
+package com.client.api.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "user_payment_info")
+public class UserPaymentInfo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_payment_info_id")
+    private Long id;
+    @Column(name = "card_number")
+    private String cardNumber;
+    @Column(name = "card_expiration_month")
+    private Long cardExpirationMonth;
+    @Column(name = "card_expiration_year")
+    private Long cardExpirationYear;
+    @Column(name = "card_security_code")
+    private String cardSecurityCode;
+    private BigDecimal price;
+    @Column(name = "dt_payment")
+    private LocalDate dtPayment;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public UserPaymentInfo() {}
+
+    public UserPaymentInfo(String cardNumber, Long cardExpirationNumber, Long cardExpirationYear, String cardSecurityCode, BigDecimal price, LocalDate dtPayment, User user) {
+        this.cardNumber = cardNumber;
+        this.cardExpirationMonth = cardExpirationNumber;
+        this.cardExpirationYear = cardExpirationYear;
+        this.cardSecurityCode = cardSecurityCode;
+        this.price = price;
+        this.dtPayment = dtPayment;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Long getCardExpirationMonth() {
+        return cardExpirationMonth;
+    }
+
+    public void setCardExpirationMonth(Long cardExpirationMonth) {
+        this.cardExpirationMonth = cardExpirationMonth;
+    }
+
+    public Long getCardExpirationYear() {
+        return cardExpirationYear;
+    }
+
+    public void setCardExpirationYear(Long cardExpirationYear) {
+        this.cardExpirationYear = cardExpirationYear;
+    }
+
+    public String getCardSecurityCode() {
+        return cardSecurityCode;
+    }
+
+    public void setCardSecurityCode(String cardSecurityCode) {
+        this.cardSecurityCode = cardSecurityCode;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public LocalDate getDtPayment() {
+        return dtPayment;
+    }
+
+    public void setDtPayment(LocalDate dtPayment) {
+        this.dtPayment = dtPayment;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
