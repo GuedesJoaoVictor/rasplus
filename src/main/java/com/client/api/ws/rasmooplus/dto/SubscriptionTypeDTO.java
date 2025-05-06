aa921bce37e21c99fe1a2e4b7c01f5b471526cb8
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-05-05 23:18:14 -0300

    feat: Adicionando dependencia
    Adicionando dependencia para validar inputs

--- src/main/java/com/client/api/ws/rasmooplus/dto/SubscriptionTypeDTO.java
+++ src/main/java/com/client/api/ws/rasmooplus/dto/SubscriptionTypeDTO.java
@@ -1,5 +1,8 @@
 package com.client.api.ws.rasmooplus.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,8 +16,13 @@ import java.math.BigDecimal;
 @Builder
 public class SubscriptionTypeDTO {
     private Long id;
+    @NotBlank(message = "Cant be null or empty")
+    @Size(min = 5, max = 30)
     private String name;
+    @Max(value = 12, message = "Access Month cant be more than 12")
     private Long accessMonth;
+    @NotBlank(message = "Cant be null or empty")
     private BigDecimal price;
+    @Size(min = 5, max = 15)
     private String productKey;
 }
